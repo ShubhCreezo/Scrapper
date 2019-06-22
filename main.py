e0e34f50e7a9da7eb0fc6ed76d248ed9fa666e8a
Shubham Karma <shubh.karma001@gmail.com>
2019-06-22 14:09:19 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# all imports
 import pandas as pd
 import time
 from pandas import ExcelWriter
@@ -25,13 +26,14 @@ def makeList1(listSource, listName):
         listName.append(elem.get_attribute('innerHTML'))
 
 
+# settings for selenium driver
 chromeOptions = Options()
 chromeOptions.add_argument("--kiosk")
-chromeOptions.add_argument("--headless")
+# comment below line for running script without GUI
+# chromeOptions.add_argument("--headless")
 
+# launching automated session
 driver = webdriver.Chrome(options=chromeOptions)
-secondDriver = webdriver.Chrome(options=chromeOptions)
-
 url = 'https://maps.google.com'
 
 # let's call google maps
@@ -44,16 +46,18 @@ def mainSite(counter, url):
     except TimeoutException:
         counter += 1
         if counter < 5:
-            driver.get(url)
+            mainSite(counter, url)
+    except:
+        print('Try again after some time!!!')
 
 
 mainSite(counter, url)
 
-# inputs
-cityName = 'Prague, Czech Republic'
+# input the city we want to search
+cityName = 'Lucerne, Switzerland'
 searchTerm = 'Things to do in ' + cityName
 
-# search for things
+# search for activities in the city
 
 element = WebDriverWait(driver, 20).until(
     EC.element_to_be_clickable((By.ID, 'searchboxinput')))
@@ -62,13 +66,8 @@ searchInput.send_keys(searchTerm)
 searchInput.send_keys(Keys.RETURN)
 print('search completed')
 
-def main(cityName):
 
-    cityName = 'Prague, Czech Republic'
-    baseURL = 'https://www.google.com/maps/search/'
-    plusURL = searchTerm.replace(' ', '+')
-    mainURL = baseURL + plusURL
-    # df = pd.DataFrame()
+def main(maindf2, cityName2, dfReturn2):
 
     # wait for results
     element = WebDriverWait(driver, 20).until(
@@ -105,22 +104,28 @@ def main(cityName):
     websiteInfoList = []
     phoneNumberList = []
     coverImageList = []
-    # firstThreeReviewsList = []
     opensAtList = []
     latitudeList = []
     longitudeList = []
 
     counter = len(activityName)
-    print(counter)
+
     for elem in range(counter-1):
         pageFound = True
+        # this clicks at one activity according to index
         try:
             activityName[elem].click()
         except ElementClickInterceptedException:
             time.sleep(2)
             activityName[elem].click()
+        except:
+            time.sleep(2)
+            activityName = driver.find_elements_by_class_name('section-result-title')
+            activityName[elem].click()
+
         print('elem ', elem)
 
+        # if any activity is taking too long to load then we skip it
         def checkPlace(pageFound):
             try:
                 element = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CLASS_NAME, "section-editorial-quote")))
@@ -128,13 +133,16 @@ def main(cityName):
             except TimeoutException:
                 print('skipped one place!!!')
                 pageFound = False
+            return pageFound
         checkPlace(pageFound)
-        # driver.implicitly_wait(15)
 
+        # now we parse all the details of any particular activity
         if pageFound:
+            # this gets page url
             pageURL = driver.current_url
             pageURLList.append(pageURL)
 
+            # this gets latitudes and longitudes
             coordinateHolder = str(pageURL).split('/')
             coordinateTemp = ''
             coordinateAvailable = True
@@ -155,21 +163,24 @@ def main(cityName):
                 latitudeList.append('unavailable')
                 longitudeList.append('unavailable')
 
+            # this gets editorial quote
             try:
                 editorialQuote = driver.find_element_by_class_name('section-editorial-quote')
                 editorialQuoteList.append(editorialQuote.find_element_by_tag_name('span').get_attribute('innerHTML'))
             except NoSuchElementException:
                 editorialQuoteList.append('NA')
 
+            # this gets address of the activity
             try:
                 address = driver.find_element_by_xpath('//*[@id="pane"]/div/div[1]/div/div/div[10]/div/div[1]/span[3]/span[3]')
-                if cityName in address.get_attribute('innerHTML'):
+                if cityName2 in address.get_attribute('innerHTML'):
                     addressList.append(address.get_attribute('innerHTML'))
                 else:
-                    addressList.append(cityName)
+                    addressList.append(cityName2)
             except NoSuchElementException:
                 addressList.append('NA')
 
+            # this gets Date and Timing info
             try:
                 daysAndTimingsWrap = driver.find_element_by_class_name('section-popular-times')
                 daysAndTimings = daysAndTimingsWrap.find_element_by_class_name('goog-menu-button-caption')
@@ -177,79 +188,103 @@ def main(cityName):
             except NoSuchElementException:
                 daysAndTimingsList.append('NA')
 
+            # this gets web info of activity
             try:
                 websiteInfo = driver.find_element_by_xpath('//*[@id="pane"]/div/div[1]/div/div/div[12]/div/div[1]/span[3]/span[3]')
                 websiteInfoList.append(websiteInfo.get_attribute('innerHTML'))
             except NoSuchElementException:
                 websiteInfoList.append('NA')
 
+            # this gets the phone number
             try:
                 phoneNumber = driver.find_element_by_xpath('//*[@id="pane"]/div/div[1]/div/div/div[13]/div/div[1]/span[3]/span[3]')
                 phoneNumberList.append(phoneNumber.get_attribute('innerHTML'))
             except NoSuchElementException:
                 phoneNumberList.append('NA')
 
+            # this gets the cover Image
             try:
-                coverImageWrap = driver.find_element_by_class_name('section-hero-header-hero')
-                coverImage = driver.find_element_by_tag_name('img')
+                coverImageWrap = driver.find_element_by_class_name('section-hero-header-image-hero')
+                coverImage = coverImageWrap.find_element_by_tag_name('img')
                 coverImageList.append(coverImage.get_attribute('src'))
             except NoSuchElementException:
                 coverImageList.append('NA')
 
+            # this gets the time at which activity starts
             try:
-                opensAt = driver.find_element_by_xpath('//*[@id="pane"]/div/div[1]/div/div/div[14]/div[1]/span[2]/span[2]')
-                opensAtList.append(opensAt.get_attribute('innerHTML'))
+                opensAtWrap = driver.find_element_by_class_name('section-info-hour-text')
+                # opensAt = driver.find_element_by_xpath('//*[@id="pane"]/div/div[1]/div/div/div[14]/div[1]/span[2]/span[2]')
+                opensAt = opensAtWrap.find_elements_by_tag_name('span')
+                opensAtList.append(opensAt[1].get_attribute('innerHTML'))
             except NoSuchElementException:
                 opensAtList.append('NA')
 
-        print('calling main URL...')
         # to go back to the main page
-        # driver.get(mainURL)
-        driver.back()
-
+        print('calling main URL...')
+        while True:
+            try:
+                driver.find_element_by_class_name('section-back-to-list-button').click()
+            except NoSuchElementException:
+                continue
+            break
+        # we wait to go back to all activities page
         try:
-            element = WebDriverWait(driver, 50).until(
+            element = WebDriverWait(driver, 200).until(
                 EC.element_to_be_clickable((By.CLASS_NAME, 'section-result-title')))
         except TimeoutException:
             time.sleep(2)
+            print('May have to try again!!!')
+        # we again prepare a list because the page is dynamic
         activityName = driver.find_elements_by_class_name('section-result-title')
 
     # Adding listName in Dataframe
     basicInfo = list(zip(activityNameList, descriptionList, activityRatingList, reviewsList, locationTypeList,
-                         pageURLList, editorialQuoteList, addressList, daysAndTimingsList, websiteInfoList, phoneNumberList,
-                         coverImageList, opensAtList, latitudeList, longitudeList))
-    df = pd.DataFrame(basicInfo, columns=['Name of Activity', 'Description', 'Ratings', 'Number of Reviews', 'Location Type'
-                                          , 'Page URL', 'Editorial Quote', 'Address', 'Days and Timings', 'Website Info',
-                                          'Phone number', 'Cover Image', 'Opens At', 'Latitude', 'Longitude'])
-
-    print('recording in excel')
-    with ExcelWriter('record.xlsx') as writer:
-        df.to_excel(writer)
-
-
-def nextPageActivities():
-    main(cityName)
-    try:
-        nextPage = driver.find_element_by_xpath('//*[@id="n7lv7yjyC35__section-pagination-button-next"]')
-        if nextPage.is_enabled():
-            print('page found')
-            time.sleep(2)
-            nextPage.click()
-            nextPageActivities()
-        else:
-            print('We are done here!!!')
-    except NoSuchElementException:
-        print('We are done here!!! lol')
-    except ElementClickInterceptedException:
-        if nextPage.is_enabled():
-            print('page found')
-            time.sleep(2)
-            nextPage.click()
-            nextPageActivities()
+                         pageURLList, editorialQuoteList, addressList, daysAndTimingsList, websiteInfoList,
+                         phoneNumberList, coverImageList, opensAtList, latitudeList, longitudeList))
+    df = pd.DataFrame(basicInfo, columns=['Name of Activity', 'Description', 'Ratings', 'Number of Reviews',
+                                          'Location Type', 'Page URL', 'Editorial Quote', 'Address', 'Days and Timings',
+                                          'Website Info', 'Phone number', 'Cover Image', 'Opens At', 'Latitude',
+                                          'Longitude'])
+    dfReturn2 = maindf2.append(df, ignore_index=True)
+    return dfReturn2
 
 
-nextPageActivities()
+maindf = pd.DataFrame(columns=['Name of Activity', 'Description', 'Ratings', 'Number of Reviews',
+                               'Location Type', 'Page URL', 'Editorial Quote', 'Address', 'Days and Timings',
+                               'Website Info', 'Phone number', 'Cover Image', 'Opens At', 'Latitude', 'Longitude'])
+dfReturn = pd.DataFrame()
+pageCounter = 0
 
+
+def nextPageActivities(maindf1, cityName1, dfReturn1):
+    dfReturn1 = main(maindf1, cityName1, dfReturn1)
+    maindf1 = dfReturn1
+    while True:
+        try:
+            nextPage = driver.find_element_by_xpath('//*[@id="n7lv7yjyC35__section-pagination-button-next"]')
+            if nextPage.is_enabled():
+                print('page found')
+                time.sleep(2)
+                nextPage.click()
+                nextPageActivities(maindf1, cityName1, dfReturn1)
+            else:
+                print('recording in excel')
+                with ExcelWriter('record.xlsx') as writer:
+                    dfReturn1.to_excel(writer)
+        except NoSuchElementException:
+            print('We are done here!!! lol')
+        except ElementClickInterceptedException:
+            if nextPage.is_enabled():
+                print('page found')
+                time.sleep(2)
+                nextPage.click()
+                nextPageActivities(maindf1, cityName1, dfReturn1)
+        except StaleElementReferenceException:
+            continue
+        break
+
+
+nextPageActivities(maindf, cityName, dfReturn)
 driver.quit()
 
 
